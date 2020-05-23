528d8c42eb93b08b1a8a212432e047dea6fd8b5f
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-23 15:25:38 -0400

    tested button and api call with test url all works well

--- src/components/CurrentLocationWeatherButton.js
+++ src/components/CurrentLocationWeatherButton.js
@@ -2,9 +2,25 @@ import React from 'react'
 
 const CurrentLocationWeatherButton = () => {
 
+    const exampleUrl = `http://api.openweathermap.org/data/2.5/weather?q=Atlanta,us&APPID=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`
+
+    const getWeatherData = () => {
+        return fetch(exampleUrl, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(weatherData => console.log(weatherData))
+        .catch(error => console.log(error))
+    }
+
     return (
         <div>
-            <button className="btn btn-primary">click me!</button>
+            <button 
+            className="btn btn-primary" 
+            onClick={e => getWeatherData(e)}
+            >click me!</button>
         </div>
     )
 }
