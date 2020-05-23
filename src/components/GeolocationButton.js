c2d828ce7223c93dd3589be4fcbfc8becc61592d
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-23 18:13:42 -0400

    GeolocationButton gets location but unable to store in local state with useState

--- src/components/GeolocationButton.js
+++ src/components/GeolocationButton.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { setCurrentLocation } from '../redux/actions.js'
 
-const GeolocationButton = () => {
+const GeolocationButton = (props) => {
+
+    let positionData
+
+    const [ latitude, setLatitude ] = useState()
+    const [ longitude, setLongitude ] = useState()
 
     const getPosition = (position) => {
-        console.log('position: ', position)
-        console.log('latitude: ', position.coords.latitude, 'longitude: ', position.coords.longitude);
+        debugger
+        // console.log('latitude: ', position.coords.latitude, 'longitude: ', position.coords.longitude);
+        positionData = position
+        console.log('position data: ', positionData)
+        // setLatitude(positionData.coords.latitude)
+        // setLongitude(position.coords.longitude)
+        // console.log(latitude, longitude)
+        debugger
     }
 
     const getCurrentLocationData = () => {
+        debugger
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(getPosition);
-        }    
+        } 
+    }
+    const handleClick = () => {
+        getCurrentLocationData()
+        debugger
+        setLatitude(positionData.coords.latitude)
+        setLongitude(positionData.coords.longitude)
+        console.log(latitude, longitude)
+        debugger
+        props.setLocation({latitude, longitude})
+        debugger
     }
 
     return (
@@ -19,7 +43,7 @@ const GeolocationButton = () => {
             <div className="col-xs-4">
                 <button 
                 className="btn btn-primary" 
-                onClick={e => getCurrentLocationData(e)}
+                onClick={() => handleClick()}
                 >Current Location</button>
             </div>
 
@@ -29,4 +53,10 @@ const GeolocationButton = () => {
        
 }
 
-export default GeolocationButton
+const mapDispatchToProps = dispatch => {
+    return {
+        setLocation: location => dispatch(setCurrentLocation(location))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(GeolocationButton)
