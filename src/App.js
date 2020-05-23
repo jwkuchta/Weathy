caadeaa2140c92b679d3915724455b60783ddb1c
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-23 14:51:57 -0400

    added and styled the button a bit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+// import React from 'react';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App" fluid>
+//       <div className="fill">
+//       <header className="panel-heading">
+//         <div className="text-center">
+//           <button className="btn btn-primary">Push me!</button>
+//         </div>
+//       </header>
+//     </div>
+//     </div>
+    
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <button className="btn btn-primary">click me!</button>
     </div>
   );
 }
