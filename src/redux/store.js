82a1ed437a636fb301db0ac82640462507750f2a
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-23 17:21:45 -0400

    removed unused code from store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,3 @@
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import rootReducer from './reducers'
-// import thunk from 'redux-thunk'
-
-// const composeEnhancers =
-//   typeof window === 'object' &&
-//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
-//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-//       // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-//     }) : compose;
-
-// const enhancer = composeEnhancers(
-//   applyMiddleware(thunk),
-//   // other store enhancers if any
-// );
-
-
-// export const store = createStore(rootReducer, enhancer)
-// export const persistor = persistStore(store)
-
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers'
 import thunk from 'redux-thunk'
