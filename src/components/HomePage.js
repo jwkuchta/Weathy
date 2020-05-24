8368e8787692ecbbdb707a4ad4b3d53026d4059f
Justina Kuchta <justyna.kuchta@gmail.com>
2020-05-24 16:48:01 -0400

    display grid somewhat fixed, everything working

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,26 +9,44 @@ const HomePage = ({ currentLocation }) => {
     return (
 
         <div className="row">
-            <div className="col-xs-5 title">
+            <div className="col-md-6 title">
                 <div>
-                    <p id="title">
+                    {/* <p id="title">
                     <h1>weather</h1>
                     <h1>checker</h1>
-                    </p>
+                    </p> */}
                 </div>
             </div>
 
-            <div className="col-xs-7 form">
-                <CityCountryForm />
-            </div>
-            
-            <div className="col-xs-4" form>
-                <CurrentLocationButton />
-            </div>
+            <div className="col-md-6 form">
+                
+                <div className="row">
+                    <div className="col-md-12">
+                    <CityCountryForm />
+                    </div>
+                </div>
 
-            <div className="row-xs-5">
-                {currentLocation !== null && <WeatherOutputContainer />}
+                <br></br><br></br>
+
+                <div className="row">
+                    <div className="col-md-4"></div>
+                    <div className="col-md-4">
+                    <CurrentLocationButton />
+                    </div>
+                    <div className="col-md-4"></div>
+                </div>
+
+                <br></br><br></br>
+
+                <div className="row">
+                    <div className="col-md-1"></div>
+                    <div className="col-md-10">
+                    {currentLocation !== null && <WeatherOutputContainer />}
                 {/* {currentLocation === null && <p id="weather-data">Location Data not available</p>} */}
+                    </div>
+                    <div className="col-md-1"></div>
+                </div>
+                
             </div>
         </div>
     )
